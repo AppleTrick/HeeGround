52ec214eddee86e2144f6cfd7bbaa1dc829abdd1
ChangHee Park <arrans9591@gmail.com>
2024-03-01 20:16:48 +0900

    페이지들 디자인

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,5 +1,12 @@
+import styles from "./footer.module.css";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>ParkDev</div>
+      <div className={styles.text}>Park Creative thoughts agency ⓒ All rights reserved</div>
+    </div>
+  );
 };
 
 export default Footer;
